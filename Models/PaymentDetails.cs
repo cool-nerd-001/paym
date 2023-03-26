4b51b74f62f73b4075d17c074f697e22b218e642
Poorna Oruganti <poorna.oruganti@STLERI.COM>
2023-03-26 16:36:34 +0530

    first commit

---
+++ Models/PaymentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentMicroservice.Models
+{
+    public class PaymentDetails
+    {
+        [Required]
+        [RegularExpression(@"^[0-9]*$")]
+        [MinLength(12)]
+        [MaxLength(12)]
+        public string? CreditCardNumber { get; set; }
+
+
+        [Required]
+        [Range(2023, 2050, ErrorMessage = "ExpiryYear Field is Required Or ExpiryYear is invalid")]
+        public int ExpiryYear { get; set; }
+
+
+        [Required]
+        [Range(100, 999, ErrorMessage = "Cvv Field is Required Or Cvv Number is invalid")]
+        public int Cvv { get; set; }
+    }
+}
